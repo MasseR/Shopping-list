b4e6aa8b5dc237244a0096733571654b2c9bf5b4
Mats Rauhala <mats.rauhala@gmail.com>
2011-03-15 12:16:24 +0200

    Remove lockfile even when errors happen

--- Transaction.hs
+++ Transaction.hs
@@ -2,12 +2,12 @@ module Transaction (withFileTransaction) where
 import System.Directory (createDirectory, removeDirectory)
 import Control.Concurrent (threadDelay)
 import System.IO.Error (isAlreadyExistsError)
+import Control.Exception (finally)
 
 tryLock file = catch (createDirectory file) (\e -> if isAlreadyExistsError e then putStrLn "Waiting" >> threadDelay 1000 >> tryLock file else return ())
 
 withFileTransaction ::  FilePath -> IO b -> IO b
 withFileTransaction file f = do
   tryLock file
-  a <- f
-  removeDirectory file
+  a <- (f) `finally` removeDirectory file
   return a
