fcfb0d1aa7f8e117575431948a5a372b8ed58cbc
Mats Rauhala <mats.rauhala@gmail.com>
2011-05-20 02:12:26 +0300

    Fix hiding
    Also, toggle it

--- js/ajax.js
+++ js/ajax.js
@@ -1,7 +1,46 @@
+show = false;
+function hideSelected()
+{
+    if(show) return;
+    var checked = $("#items > li > input:checked");
+    $("#hiddenitems").show().text(checked.size() + " items hidden");
+    checked.each(function() {
+	$(this).parent().hide();
+    });
+}
+
+function showAll()
+{
+    $("#items > li").each(function() {
+	$(this).show();
+    });
+}
+
+function bindHide()
+{
+    $("#items > li > input").each(function(idx) {
+	$(this).change(function() {
+	    hideSelected();
+	})
+    });
+}
+
+function toggle()
+{
+    show = !show;
+    if(show)
+	showAll();
+    else
+	hideSelected();
+}
+
 $(document).ready(function() {
+    $("#show").click(toggle);
+    bindHide();
     $("#append").submit(function() {
 	$.post("ostoslista.cgi?mode=ajaxadd", $(this).serialize(), function(data) {
 	    $("#items").html(data);
+	    bindHide();
 	});
 	return false;
     });
